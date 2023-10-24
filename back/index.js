16393835eaf0fa26dc8860a4626207f29ba3b43a
Maria Eduarda Andrade da Silva <107086291+MariaAndradeS@users.noreply.github.com>
2023-10-24 18:02:40 -0300

    Add files via upload

---
+++ back/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import rotasUsuario from "./rotas/rotasUsuario.js"
+
+// const express = require("express")
+// const cors = require("cors")
+// const rotas = require("./rotasUsuario")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/user", rotasUsuario)
+
+app.listen(8080)
+
+
