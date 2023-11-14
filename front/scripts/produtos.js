497bef58f8203aa2050c1f1045a6aa2f332b673a
Maria Eduarda Andrade da Silva <107086291+MariaAndradeS@users.noreply.github.com>
2023-11-14 16:14:41 -0300

    Add o header e o footer nas paginas

---
+++ front/scripts/produtos.js
@@ -0,0 +1,11 @@
+// Maria: Exibir e fechar o menu do usuário
+const userIcon = document.getElementById('userIcon');
+const userMenu = document.getElementById('userMenu');
+
+userIcon.addEventListener('click', function () {
+    if (userMenu.style.display === 'block') {
+        userMenu.style.display = 'none';
+    } else {
+        userMenu.style.display = 'block';
+    }
+});
