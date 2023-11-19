6e1f9bab0d36a87b41e723a804d6cfcd438cdc97
Maria Eduarda Andrade da Silva <eaemariasilva@icloud.com>
2023-11-19 20:49:37 -0300

    Submenu arrumado

--- front/scripts/home.js
+++ front/scripts/home.js
@@ -10,6 +10,30 @@ userIcon.addEventListener('click', function () {
     }
 });
 
+const submenus = document.querySelectorAll(".submenu");
+
+        submenus.forEach(element => {
+            element.addEventListener("mouseover", () => {
+                element.querySelector(".submenu-options").classList.add("display-active");
+            })
+
+            element.addEventListener("mouseout", () => {
+                element.querySelector(".submenu-options").classList.remove("display-active");
+            })
+        });
+
+        const submenuOptions = document.querySelectorAll(".submenu-option");
+
+        submenuOptions.forEach(element => {
+            element.addEventListener("mouseover", () => {
+                element.querySelector(".submenu-options-inner").classList.add("display-active");
+            })
+
+            element.addEventListener("mouseout", () => {
+                element.querySelector(".submenu-options-inner").classList.remove("display-active");
+            })
+        });
+
 //Lógica dos botões - slide (Luiza)
 let slideIndex = 0;
 const slides = document.querySelectorAll('.grid-item-dicas');
