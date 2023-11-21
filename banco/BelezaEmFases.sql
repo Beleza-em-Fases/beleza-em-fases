33eeab75eca3be5b16efd388690d9ccf65d07687
Maria Eduarda Andrade da Silva <eaemariasilva@icloud.com>
2023-11-21 14:09:37 -0300

    AAAA

--- banco/BelezaEmFases.sql
+++ banco/BelezaEmFases.sql
@@ -88,8 +88,37 @@ CREATE TABLE Produtos(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(255) NOT NULL,
 marca VARCHAR(255) NOT NULL,
-descricao VARCHAR(255) NOT NULL,
+descricao VARCHAR(1000) NOT NULL,
 funcao VARCHAR(255) NOT NULL,
 preco DECIMAL(10, 2),
 imagem VARCHAR(255) NOT NULL
 );
+
+INSERT INTO Produtos(nome, marca, descricao, funcao, preco, imagem) VALUES(
+'Máscara Morte Súbita', 'Lola Cosmetics', 'Máscara de hidratação para cabelos danificados. Lola Cosmetics Morte Súbita recupera as pontas duplas, hidrata e dá brilho e a suavidade. A Máscara Morte Súbita, de Lola Cosmetics, conta com ingredientes naturais que fortalecem a estrutura dos fios contra a quebra e protegem os cabelos dos danos causados pelo calor e exposição aos raios ultravioletas. O creme tem nutrientes que resgatam a hidratação natural da fibra capilar, o brilho e a suavidade dos fios', 'Hidronutrição', 63.90, 'https://tfdfn2.vtexassets.com/arquivos/ids/220906/mascara-lola-morte-subita-450g.jpg?v=638168148221100000'
+);
+
+-- Possiveis Selects para a tabela produto
+SELECT * FROM Produtos;
+
+SELECT * FROM Produtos
+ORDER BY nome;
+
+SELECT * FROM Produtos
+WHERE marca = 'Lola Cosmetics'
+ORDER BY preco;
+
+SELECT * FROM Produtos
+WHERE nome = 'Máscara Morte Súbita';
+
+SELECT * FROM Produtos
+WHERE marca = 'Lola Cosmetics';
+
+SELECT * FROM Produtos
+WHERE marca = 'Lola Cosmetics';
+
+SELECT * FROM Produtos
+WHERE funcao LIKE '%Hidratação%';
+
+SELECT * FROM Produtos
+WHERE preco < 50;
