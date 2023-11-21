138caadc70ae4a1b07700ddfb6dc748e391087c5
Maria Eduarda Andrade da Silva <eaemariasilva@icloud.com>
2023-11-21 14:20:05 -0300

    Criando as rotas necessarias

--- back/index.js
+++ back/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
 import rotasUsuario from "./rotasUsuario.js"
+import rotasForum from "./rotasForum.js"
+import rotasProdutos from "./rotasProdutos.js"
+import rotasQuiz from "./rotasQuiz.js"
 
 const app = express()
 
@@ -8,7 +11,11 @@ app.use(express.json()) //Maria: Recebe pacotes
 app.use(cors())
 
 app.use("/usuario", rotasUsuario) //Maria: Usa a rota /usuario
+app.use("/forum", rotasForum) //Maria: Usa a rota /forum
+app.use("/produtos", rotasProdutos) //Maria: Usa a rota /produtos
+app.use("/quiz", rotasQuiz) //Maria: Usa a rota /quiz
 
-app.listen(3030, () => {
-    console.log('Servidor rodando na porta 3030');
+const port = 3030
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 }); //Maria: No meu computador a porta 8080 não estava funcionando
