a08cb52308f2c9e50a50706210f9451f9c285af5
Maria Eduarda Andrade da Silva <eaemariasilva@icloud.com>
2023-11-22 01:34:22 -0300

    REDEFINIR SENHA FUNCIONANDO

--- front/scripts/redefinirSenha.js
+++ front/scripts/redefinirSenha.js
@@ -10,52 +10,67 @@ function buscarEmail() {
         "email": email.value
     }
     console.log(JSON.stringify(dados))
-    
-        return fetch("http://localhost:3030/usuario/buscar-email", {
-            method: "POST",
-            body: JSON.stringify(dados),           
-            headers:{
-                "Content-Type": "application/json"
-            }}) 
-        .then(resposta => resposta.json())
-        .then(resultado => {
-            console.log("Retorna:" + resultado );
-            return (resultado);
-        })        
-    }
 
-    formNovaSenha.addEventListener('submit', function (event){
-        event.preventDefault()
-        console.log("Verificando email");
-    
-        buscarEmail()
-            .then(verificado => {
-                console.log("Resultado: " + verificado);
-    
-                if (!verificado){
-                    console.log(email.value)
-                    event.stopPropagation()
-                    console.log("Email não existe")
-                    
-                }
-                else{
-                    let senhaNova = {
-                        "senha": senha.value
-                    }
-                    console.log(JSON.stringify(senhaNova))
-                    
-                        fetch("http://localhost:3030/usuario/redefinir/"+email.value, {
-                            method: "PUT",
-                            body: JSON.stringify(senhaNova),           
-                            headers:{
-                                "Content-Type": "application/json"
-                            }}) 
-                        .then(resposta => resposta.json())
-                        .then(resultado =>  window.location.href = "login.html")
-                }
-            })
-            
-            .catch(erro => {
-                console.error("Erro ao buscar usuário:", erro);
-            });
+    return fetch("http://localhost:3030/usuario/buscar-email", {
+        method: "POST",
+        body: JSON.stringify(dados),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
+    .then(resposta => {
+        if (!resposta.ok) {
+            throw new Error("Erro na solicitação: " + resposta.status);
+        }
+        return resposta.json();
+    })
+    .then(resultado => {
+        console.log("Retorna:" + resultado);
+        return resultado;
+    })
+    .catch(erro => {
+        console.error("Erro ao buscar usuário:", erro);
+    });
+}
+
+formNovaSenha.addEventListener('submit', function (event) {
+    event.preventDefault()
+    console.log("Verificando email");
+
+    buscarEmail()
+        .then(verificado => {
+            console.log("Resultado: " + verificado);
+
+            if (!verificado) {
+                console.log(email.value)
+                event.stopPropagation()
+                console.log("Email não existe");
+            } else {
+                let senhaNova = {
+                    "senha": senha.value
+                }
+                console.log(JSON.stringify(senhaNova))
+
+                fetch("http://localhost:3030/usuario/redefinir/" + email.value, {
+                    method: "PUT",
+                    body: JSON.stringify(senhaNova),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(resposta => {
+                    if (!resposta.ok) {
+                        throw new Error("Erro na solicitação: " + resposta.status);
+                    }
+                    return resposta.json();
+                })
+                .then(resultado => window.location.href = "login.html")
+                .catch(erro => {
+                    console.error("Erro na redefinição de senha:", erro);
+                });
+            }
+        })
+        .catch(erro => {
+            console.error("Erro ao buscar usuário:", erro);
+        });
+});
